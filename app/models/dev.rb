257b7d2fe9674d757436ecf8dd957ed67849cb12
Elizabeth Klenczar <eklenczar@gmail.com>
2022-08-18 18:20:22 -0400

    commit tree 29eadfcf4aa4d5e26a1a3f9b64e0862fe92fbf4a

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
-class Dev < ActiveRecord::Base
+require "pry"
 
+class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def freebies
+        self.freebies.map do |freebie|
+            binding.pry
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -11,7 +15,10 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+Freebie.create(item_name: "Pen", value: "1")
+Freebie.create(item_name: "Poster", value: "8")
+Freebie.create(item_name: "Stress Ball", value: "5")
+Freebie.create(item_name: "Water Bottle", value: "10")
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
