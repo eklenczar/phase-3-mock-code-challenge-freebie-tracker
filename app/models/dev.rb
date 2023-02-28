046874bf4f478331f6f3aca10b61212646d09120
Elizabeth Klenczar <eklenczar@gmail.com>
2023-02-28 14:54:44 -0500

    completed with right answers

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.find_by(item_name: item_name) ? true : false
+    end
+
+    def give_away(dev, freebie)
+        if self.id == freebie.dev.id
+            freebie.update(dev_id: dev.id)
+        else
+            nil
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,24 @@
+Company.destroy_all
+Dev.destroy_all
+Freebie.destroy_all
+
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+meseeks = Dev.create(name: "Mr. Meseeks")
+gazor = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+pen = Freebie.create(item_name: "Pen", value: "1", dev_id: rick.id, company_id: google.id)
+poster = Freebie.create(item_name: "Poster", value: "8", dev_id: rick.id, company_id: google.id)
+stress_ball = Freebie.create(item_name: "Stress Ball", value: "5", dev_id: rick.id, company_id: google.id)
+water_bottle = Freebie.create(item_name: "Water Bottle", value: "10", dev_id: rick.id, company_id: google.id)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
